8ecc709db5990db8c2048cc4f6545a9b3d03fe06
IvanOrsh <ksironov@mail.ru>
2023-02-12 23:39:54 +0300

    password check implemented

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,6 +10,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserResponseInterface } from './types/userResponse.interface';
 import { UsersService } from './users.service';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @ApiTags()
 @Controller()
@@ -22,6 +23,15 @@ export class UsersController {
     @Body('user') createUserDto: CreateUserDto,
   ): Promise<UserResponseInterface> {
     const user = await this.usersService.createUser(createUserDto);
+    return this.usersService.buildUserResponse(user);
+  }
+
+  @Post('users/login')
+  @UsePipes(new ValidationPipe())
+  async login(
+    @Body('user') loginUserDto: LoginUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.usersService.login(loginUserDto);
     return this.usersService.buildUserResponse(user);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,11 +2,13 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { sign } from 'jsonwebtoken';
+import { compare } from 'bcrypt';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { JWT_SECRET } from '../config';
 import { UserResponseInterface } from './types/userResponse.interface';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -37,6 +39,38 @@ export class UsersService {
     const user = await this.usersRepository.create(createUserDto);
 
     return await this.usersRepository.save(user);
+  }
+
+  async login(loginUserDto: LoginUserDto): Promise<User> {
+    const userByEmail = await this.usersRepository.findOne({
+      where: {
+        email: loginUserDto.email,
+      },
+      select: ['id', 'username', 'email', 'bio', 'image', 'password'],
+    });
+
+    if (!userByEmail) {
+      throw new HttpException(
+        `${loginUserDto.email} not found`,
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    const isPasswordCorrect = await compare(
+      loginUserDto.password,
+      userByEmail.password,
+    );
+
+    if (!isPasswordCorrect) {
+      throw new HttpException(
+        `Password is not valid`,
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    delete userByEmail.password;
+
+    return userByEmail;
   }
 
   private generateJwt(user: User): string {
