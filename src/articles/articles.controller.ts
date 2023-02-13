faf00ec6157366c6fd122cfa23573ab77888b240
IvanOrsh <ksironov@mail.ru>
2023-02-14 01:51:43 +0300

    createArticle (Post /articles) added

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+
 import { ArticlesService } from './articles.service';
+import { AuthGuard } from '../users/guards/auth.guard';
+import { CurrentUser } from '../users/decorators/user.decorator';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { User } from '../users/entities/user.entity';
+import { Artilce } from './entities/article.entity';
 
 @Controller('articles')
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
+
   @Post()
-  async createArtile() {
-    return await this.articlesService.createArticle();
+  @UseGuards(AuthGuard)
+  async createArtile(
+    @CurrentUser() currentUser: User,
+    @Body('article') body: CreateArticleDto,
+  ): Promise<Artilce> {
+    return await this.articlesService.createArticle(currentUser, body);
   }
 }
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+
+import { User } from '../users/entities/user.entity';
+import { CreateArticleDto } from './dto/create-article.dto';
 import { Artilce } from './entities/article.entity';
 
 @Injectable()
@@ -10,7 +13,16 @@ export class ArticlesService {
     private readonly articlesRepository: Repository<Artilce>,
   ) {}
 
-  async createArticle() {
-    return 'ArticlesService -> crateArtilce()';
+  async createArticle(currentUser: User, body: CreateArticleDto) {
+    const article = await this.articlesRepository.create(body);
+
+    if (!article.tagList) {
+      article.tagList = [];
+    }
+
+    article.author = currentUser;
+    article.slug = 'testing';
+
+    return await this.articlesRepository.save(article);
   }
 }
