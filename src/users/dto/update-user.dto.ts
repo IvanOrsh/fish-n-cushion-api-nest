118815a3d35f6789ce8def8a23d87d208529a38c
IvanOrsh <ksironov@mail.ru>
2023-02-14 01:28:01 +0300

    minor fix

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -3,21 +3,21 @@ import { IsEmail, IsString, IsOptional } from 'class-validator';
 export class UpdateUserDto {
   @IsEmail()
   @IsOptional()
-  readonly email: string;
+  readonly email?: string;
 
   @IsString()
   @IsOptional()
-  readonly username: string;
+  readonly username?: string;
 
   @IsString()
   @IsOptional()
-  readonly password: string;
+  readonly password?: string;
 
   @IsString()
   @IsOptional()
-  readonly image: string;
+  readonly image?: string;
 
   @IsString()
   @IsOptional()
-  readonly bio: string;
+  readonly bio?: string;
 }
