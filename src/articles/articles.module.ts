4e6b49aa363a0e8258ae3ab32d31d71e36f7a5ff
IvanOrsh <ksironov@mail.ru>
2023-02-14 00:49:25 +0300

    Article Entity wired to article module

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticlesController } from './articles.controller';
 import { ArticlesService } from './articles.service';
+import { Artilce } from './entities/article.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Artilce])],
   controllers: [ArticlesController],
   providers: [ArticlesService],
 })
